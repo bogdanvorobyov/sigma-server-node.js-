9d2fe5f0358cc1825fba65b34f88a4ae94c8e4c3
Bogdan <bogdanvo12345@gmail.com>
2021-03-02 18:37:06 +0200

    deploy223

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
+mongoose.set('MongoError', false);
 
 app.use(corsMiddleware)
 
